cd300112eda4134e8a9cc49b1d27018885b7a261
JordanGoetze <jordan.goetze@target.com>
2018-08-13 10:59:06 -0500

    gets state updating working

--- client/src/Flow.js
+++ client/src/Flow.js
@@ -18,118 +18,20 @@ class Flow extends Component {
         super(props);
 
         this.state = {
-            steps: null,
-            states: null,
-            pipeline: null,
         }
 
+        this.steps = null;
+        this.states = null;
+        this.pipeline = null;
 
-        //1) setup the diagram this.engine
-        this.engine = new DiagramEngine();
-        this.engine.installDefaultFactories();
-
-        //2) setup the diagram model
-        this.model = new DiagramModel();
-
-        //5) load model into this.engine
-        this.engine.setDiagramModel(this.model);
-
-        this.model.setLocked(true);
-        this.graph_props = {
-            allowLooseLinks: false,
-            allowCanvasTranslation: false,
-            allowCanvasZoom: false,
-            maxNumberPointsPerLink: 0,
-        };
-
-        props.socket.on('send_pipelines', ((payload) => {
-
-            //TODO based on pipeline, generate graph
-
-            var model = this.engine.getDiagramModel();
-
-            let pipelines = JSON.parse(payload.pipelines)
-
-            var steps = pipelines.steps;
-            var states = pipelines.states;
-
-            var new_steps = []
-
-            for(var i=0; i<steps.length; i++){
-                let step = steps[i];
-
-                var current_state = null
-
-                for (var state of states){
-                    if (state.name === step.initial_state){
-                        current_state = state;
-                        break
-                    }
-                }
-
-                let new_node = new DefaultNodeModel(step.name, current_state !== null ?current_state.color : "rgb(0,255,0)");
-                new_node.setPosition(100*i, 50*i);
-                let out_port = new_node.addOutPort("Out");
-                let in_port = new_node.addInPort("In");
-
-                new_steps.push({
-                    "name": step.name,
-                    "node": new_node,
-                    "in": in_port,
-                    "out": out_port,
-                })
-            }
-
-            //Add links
-            var new_links = []
-            for(var segment of pipelines.pipeline){
-
-                // Get current step
-                var current_step = null;
-                for(var step of new_steps){
-                    if(step.name === segment.step){
-                       current_step = step;
-                        break;
-                    }
-                }
-                if(current_step === null) continue;
-
-                for(var transition of segment.transitions){
-
-                    // get node to transition to
-                    var transition_to = null;
-                    for(var step of new_steps){
-                        if(step.name == transition.go_to){
-                            transition_to = step;
-                            break
-                        }
-                    }
-                    if(transition_to === null) continue;
-
-                    //create link
-                    let new_link = current_step.out.link(transition_to.in);
-                    new_link.addLabel(transition.state);
-
-                    new_links.push(new_link)
-                }
-
-                model.addAll(...new_steps.map(e=>e.node),...new_links)
-                let distributedModel = this.getDistributedModel(this.engine, model);
-                distributedModel.setLocked(true);
-                this.engine.setDiagramModel(distributedModel);
+        this.initGraph();
 
 
-            }
-
-            this.setState({
-                states: payload.pipelines.states,
-                steps: payload.pipelines.steps,
-                pipeline: payload.pipelines.pipeline,
-            })
-
-            this.forceUpdate();
-
+        props.socket.on('send_pipelines', ((payload)=>{
+            this.updatePipeline(payload);
+            this.renderPipeline();
         }).bind(this));
+        props.socket.on('notify_state_change', this.processStateUpdate.bind(this));
     }
 
     getDistributedModel(engine, model) {
@@ -149,6 +51,123 @@ class Flow extends Component {
             </div>
         );
     }
+
+    initGraph(){
+        //1) setup the diagram this.engine
+        this.engine = new DiagramEngine();
+        this.engine.installDefaultFactories();
+
+        //2) setup the diagram model
+        this.model = new DiagramModel();
+
+        //5) load model into this.engine
+        this.engine.setDiagramModel(this.model);
+
+        this.model.setLocked(true);
+        this.graph_props = {
+            allowLooseLinks: false,
+            allowCanvasTranslation: false,
+            allowCanvasZoom: false,
+            maxNumberPointsPerLink: 0,
+        };
+    }
+
+    updatePipeline(payload){
+        let pipelines = JSON.parse(payload.pipelines)
+
+        this.steps = pipelines.steps;
+        this.states = pipelines.states;
+        this.pipeline = pipelines.pipeline
+    }
+
+    processStateUpdate(payload){
+        let data  = JSON.parse(payload);
+
+        for(var step of this.steps){
+            if(step.name === data.step){
+                step.state = data.state
+                break;
+            }
+        }
+
+        this.initGraph();
+        this.renderPipeline();
+    }
+
+    renderPipeline(){
+        var model = this.engine.getDiagramModel();
+
+        var new_steps = []
+
+        for(var i=0; i<this.steps.length; i++){
+            let step = this.steps[i];
+
+            var current_state = null
+
+            for (var state of this.states){
+                if (state.name === step.state){
+                    current_state = state;
+                    break
+                }
+            }
+
+            let new_node = new DefaultNodeModel(step.name, current_state !== null ?current_state.color : "rgb(0,255,0)");
+            new_node.setPosition(100*i, 50*i);
+            let out_port = new_node.addOutPort("Out");
+            let in_port = new_node.addInPort("In");
+
+            new_steps.push({
+                "name": step.name,
+                "node": new_node,
+                "in": in_port,
+                "out": out_port,
+            })
+        }
+
+        //Add links
+        var new_links = []
+        for(var segment of this.pipeline){
+
+            // Get current step
+            var current_step = null;
+            for(var step of new_steps){
+                if(step.name === segment.step){
+                   current_step = step;
+                    break;
+                }
+            }
+            if(current_step === null) continue;
+
+            for(var transition of segment.transitions){
+
+                // get node to transition to
+                var transition_to = null;
+                for(var step of new_steps){
+                    if(step.name == transition.go_to){
+                        transition_to = step;
+                        break
+                    }
+                }
+                if(transition_to === null) continue;
+
+                //create link
+                let new_link = current_step.out.link(transition_to.in);
+                new_link.addLabel(transition.state);
+
+                new_links.push(new_link)
+            }
+
+            model.addAll(...new_steps.map(e=>e.node),...new_links)
+            let distributedModel = this.getDistributedModel(this.engine, model);
+            distributedModel.setLocked(true);
+            this.engine.setDiagramModel(distributedModel);
+
+
+        }
+
+        this.forceUpdate();
+    }
+
 }
 
 export default socketConnect(Flow);
--- server/sydney.py
+++ server/sydney.py
@@ -10,9 +10,61 @@ socketio = SocketIO(app)
 with open("../schema.json", "r") as f:
     pipelines = json.load(f)
 
+for step in pipelines["steps"]:
+    step["state"] = step["initial_state"]
+
 @socketio.on('connect')
 def on_connect():
     socketio.emit('send_pipelines', {'pipelines': json.dumps(pipelines)})
+
+## ROUTES
+
+@app.route('/pipelines')
+def get_pipelines():
+    return json.dumps(pipelines)
+
+@app.route('/state/<step>')
+def get_state(step):
+
+    step = get_step(step)
+    if not step: return "Invalid step"
+
+    return step["state"]
+
+
+@app.route('/notify/<step>/<state>')
+def notify_state_change(step, state):
+
+    state = get_state(state)
+    if not state: return "Invalid state"
+
+    step = get_step(step)
+    if not step: return "Invalid step"
+
+    step["state"] = state["name"]
+
+    socketio.emit("notify_state_change", json.dumps({
+        "step": step["name"],
+        "state": state["name"]
+    }))
+
+    return "Updated {0} to state {1}".format(step["name"], state["name"])
+
+
+# HELPERS
+
+def get_state(state_name):
+    for state in pipelines["states"]:
+        if state["name"] == state_name:
+            return state
+    return None
+
+def get_step(step_name):
+    for step in pipelines["steps"]:
+        if step["name"] == step_name:
+            return step
+    return None
+
 
 if __name__ == '__main__':
     socketio.run(app)
